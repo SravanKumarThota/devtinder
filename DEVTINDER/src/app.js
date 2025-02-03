beb08a3e1b30bac29f146b319744f7bf396ce090
SravanThota <thotasravankumar099@gmail.com>
2025-02-03 23:16:56 +0530

    apis and some validations

--- DEVTINDER/src/app.js
+++ DEVTINDER/src/app.js
@@ -3,13 +3,81 @@ const User = require('./models/user')
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-app.post('/signup', async (req, res) => {
-  const userData = req.body
-  const user = new User(userData);
-  await user.save();
-  res.send("User added Successfully...");
+
+app.post("/signup", async (req, res) => {
+  const userData = req.body;
+  try {
+    const createdUser = await User.create(userData);
+    res.send(createdUser);
+  } catch (err) {
+    res.status(400).send(err.message)
+  }
 })
+
+app.get('/user/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const userDoc = await User.findById(userId);
+    if (!userDoc) {
+      res.status(404).send("User not found");
+    }
+    res.send(userDoc);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+})
+
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch(err) {
+    res.status(400).send(err.message);
+  }
+})
+
+app.post('/user', async (req, res) => {
+  const email = req.body?.email;
+  try {
+    if (!email)
+      throw new Error("Provide a valid email");
+    const userDoc = await User.find({ emailId: email });
+    if (!userDoc) {
+      throw new Error("User Not found");
+    }
+    res.send(userDoc);
+  } catch (err) {
+    
+  }
+})
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) => {
+      ALLOWED_UPDATES.includes(k);
+    });
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after", 
+      runValidators:true
+    })
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED: " + err.message);
+  }
+});
+
+
 
 connectDB()
   .then(() => {
--- DEVTINDER/src/models/user.js
+++ DEVTINDER/src/models/user.js
@@ -1,26 +1,69 @@
 const mongoose = require("mongoose");
+var validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      min: 3,
+      max: 50,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      lowercase: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Please enter a valid Gender");
+        }
+      },
+    },
+    emailId: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Please enter a valid Email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Please enter a Strong Password.");
+        }
+      },
+    },
+    imageUrl: {
+      type: String,
+      default:
+        "https://www.ihna.edu.au/blog/wp-content/uploads/2022/10/user-dummy.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Please enter a Valid Image Url");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
 // module.exports = mongoose.model('User', userSchema,'users'); -- this would be the custom name for the collections
-
